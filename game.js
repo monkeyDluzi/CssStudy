07ad7a26839eb4c64d7b05aab0f3090422f5c50e
monkeyDluzi <Luziandre588@gmail.com>
2026-06-15 11:31:51 -0400

    Fix button position and event listener in game.js

--- game.js
+++ game.js
@@ -1,10 +1,9 @@
-const catchMeButton = document.getElementById("catchMeButton")
-
-
+const catchMeButton = document.getElementById("catchMeButton");
 
 function move() {
-catchMeButton.style.left = "600px";
- catchMeButton.style.top = "600px";
+  catchMeButton.style.left = "300px";
+  catchMeButton.style.top = "300px";
 }
-catchMeButton.addEventListener("click", function();
+
+catchMeButton.addEventListener("click", move);;
   
